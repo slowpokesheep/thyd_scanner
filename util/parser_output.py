777181430cc49f3a69846a4975e50ba00a4a22f2
slowpoke <hjaltigeir@gmail.com>
2020-02-21 18:57:34 +0000

    final

--- util/parser_output.py
+++ util/parser_output.py
@@ -29,13 +29,5 @@ for line in sys.stdin:
     else:
       sys.stdout.write(line)
 
-  """
-  if (line[:2] == '-1'):
-    # Bright red and reset
-    sys.stdout.write(f'\033[1;31m{line}\033[0;0m')
-  else:
-    sys.stdout.write(line)
-  """
-
 sys.stdout.write('-' * 80)
 sys.stdout.write('\n')
